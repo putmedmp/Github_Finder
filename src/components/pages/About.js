de717fedfe9a800ffb0827a72d1bedd6122647f6
Mateusz Polakiewicz <66308893+putmedmp@users.noreply.github.com>
2021-03-23 17:25:27 +0100

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+import React from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About this app</h1>
+            <p>App to search Github users</p>
+            <p>Version 1.0.0</p>
+        </Fragment>
+    )
+}
+
+export default About
